2b74329702d2c5fc92e60e8a4034e7ee8368c871
Derek Scruggs <me@derekscruggs.com>
2014-10-26 11:15:36 -0500

    Added 301 redirect to root

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+$s = $_SERVER;
+$use_forwarded_host=false;
 
+$ssl = (!empty($s['HTTPS']) && $s['HTTPS'] == 'on') ? true:false;
+$sp = strtolower($s['SERVER_PROTOCOL']);
+$protocol = substr($sp, 0, strpos($sp, '/')) . (($ssl) ? 's' : '');
+$port = $s['SERVER_PORT'];
+$port = ((!$ssl && $port=='80') || ($ssl && $port=='443')) ? '' : ':'.$port;
+$host = ($use_forwarded_host && isset($s['HTTP_X_FORWARDED_HOST'])) ? $s['HTTP_X_FORWARDED_HOST'] : (isset($s['HTTP_HOST']) ? $s['HTTP_HOST'] : null);
+$host = isset($host) ? $host : $s['SERVER_NAME'] . $port;
+$url = $protocol . '://' . $host . '/blog';
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
+header("Location:$url",true, '301');
+die();
