c58768e672f6b7dc610cc5b9fcd7a8051c3c9d50
Derek Scruggs <me@derekscruggs.com>
2014-11-04 22:09:17 -0500

    Added new index

---
+++ blog/wp-content/themes/stanleywp/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Index Template
+ *
+ *
+ * @file           index.php
+ * @package        StanleyWP 
+ * @author         Brad Williams & Carlos Alvarez 
+ * @copyright      2011 - 2014 Gents Themes
+ * @license        license.txt
+ * @version        Release: 3.0.3
+ * @link           http://codex.wordpress.org/Theme_Development#Index_.28index.php.29
+ * @since          available since Release 1.0
+ */
+?>
+<?php get_header(); ?>
+<?php include('list_body.php'); ?>
+
+<?php get_footer(); ?>
