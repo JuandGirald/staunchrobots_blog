193ffc558f421bf44b0bba68ae52184583a02988
Derek Scruggs <me@derekscruggs.com>
2014-11-05 12:53:35 -0700

    Fixing break to site url

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -25,6 +25,9 @@ if ($development) {
 }
 
 
+define('WP_HOME','http://statusrobotsblog.herokuapp.com/blog');
+define('WP_SITEURL','http://www.staunchrobots.com/blog');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', trim($db["path"],"/"));
