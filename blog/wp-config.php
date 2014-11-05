ef7cc3c41a109dbce3b79194fd00fab12f2a4107
Derek Scruggs <me@derekscruggs.com>
2014-11-05 13:07:31 -0700

    More troubleshooting

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -26,7 +26,7 @@ if ($development) {
 
 
 define('WP_HOME','http://staunchrobotsblog.herokuapp.com/blog');
-define('WP_SITEURL','http://www.staunchrobots.com/blog');
+define('WP_SITEURL','http://staunchrobotsblog.herokuapp.com/blog');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
